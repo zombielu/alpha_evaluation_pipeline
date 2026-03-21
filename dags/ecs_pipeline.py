1c7df7b4310cc8ace28ff7d4a3b5d8b4c5b167e3
zombielu <ludaiche@gmail.com>
2026-03-21 15:35:02 -0400

    modify - complete version of deploy.yml

--- dags/ecs_pipeline.py
+++ dags/ecs_pipeline.py
@@ -3,7 +3,7 @@ from airflow.providers.amazon.aws.operators.ecs import EcsRunTaskOperator
 from datetime import datetime
 
 with DAG(
-    dag_id="alpha_evaluation_pipeline_v9",
+    dag_id="alpha_evaluation_pipeline",
     start_date=datetime(2026,3,19),
     schedule="@daily",
     catchup=False,
